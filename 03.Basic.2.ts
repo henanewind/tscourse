f62b6092f4625b051b05b98e0ff92750cbc1c9c5
henanewind <zzwonder2009@126.com>
2021-05-11 16:57:20 +0800

    + 修改变化

--- 03.Basic.2.ts
+++ 03.Basic.2.ts
@@ -3,4 +3,36 @@ let result: unknown;
 if (typeof result === 'number') {    
     result.toFixed(); // 提示 ts(2571)
 }
+// void undefined null 
+// let undeclared: undefined = undefined; // 鸡肋
 
+// let nullable: null = null; // 鸡肋
+
+// const userInfo: {
+
+//     id?: number;
+  
+//   } = {};
+  
+//   let undeclared: undefined = undefined;
+  
+//   let unusable: void = undefined;
+  
+//   unusable = undeclared; // ok
+  
+//   undeclared = unusable; // ts(2322)
+  
+const userInfo: {
+
+    id?: number;
+  
+    name?: null | string
+  
+  } = { id: 1, name: 'Captain' };
+  
+  if (userInfo.id !== undefined) { // Type Guard
+  
+    userInfo.id.toFixed(); // id 的类型缩小成 string
+  
+  }
+  
