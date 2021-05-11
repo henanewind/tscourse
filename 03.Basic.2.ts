cc8e4cf8010dc50084759c819470dcbb028ff629
henanewind <zzwonder2009@126.com>
2021-05-11 19:02:54 +0800

    * 类型断言

--- 03.Basic.2.ts
+++ 03.Basic.2.ts
@@ -35,4 +35,34 @@ const userInfo: {
     userInfo.id.toFixed(); // id 的类型缩小成 string
   
   }
+
+// 非空断言、类型守卫使用  !  
+userInfo.id!.toFixed(); // ok，但不建议
+
+userInfo.name!.toLowerCase() // ok，但不建议
+// 建议使用  ? 单问号（Optional Chain）、?? 双问号（空值合并） 
+
+userInfo.id?.toFixed(); // Optional Chain
+const myName = userInfo.name ?? `my name is ${userInfo.name}`; // 空值合并 类似 condition ? a : b
+
+function ThrowError(msg: string): never {
+
+    throw Error(msg);
   
+}
+
+/** str 类型是 '"str"' */
+
+let str = 'str' as const;
+
+/** readOnlyArr 类型是 'readonly [0, 1]' */
+
+const readOnlyArr = [0, 1] as const;
+
+let mayNullOrUndefinedOrString: null | undefined | string;
+
+mayNullOrUndefinedOrString!.toString(); // ok
+
+mayNullOrUndefinedOrString.toString(); // ts(2531)
+
+    
