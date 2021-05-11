fe52d355031afe87d95890b054cd6012de42ad35
henanewind <zzwonder2009@126.com>
2021-05-11 16:13:00 +0800

    + 增加TS代码

---
+++ 03.Basic.2.ts
@@ -0,0 +1,6 @@
+let result: unknown;
+
+if (typeof result === 'number') {    
+    result.toFixed(); // 提示 ts(2571)
+}
+
---
+++ HelloWorld.ts
@@ -0,0 +1,6 @@
+// tsc HelloWorld.ts --strict --alwaysStrict false --watch
+// ts-node HelloWord.ts
+
+const trueNum: number = 42;
+
+// const fakeNum: number = "42"; // ts(2322) Type 'string' is not assignable to type 'number'.
