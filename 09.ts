d6d5870e4273652f48eb1020558d63689a20e026
henanewind <zzwonder2009@126.com>
2021-06-19 17:49:10 +0800

    + 增加10.ts泛型课程

--- 09.ts
+++ 09.ts
@@ -1,29 +1,21 @@
-enum Day {
-    SUNDAY,
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY
+declare let $: any;
+$('#id').addClass('show');
+// 外部枚举，declare类似于c++中的extern关键字
+// 说明某一个类型在其它地方定义过，此处只是说明(声明)一下
+declare enum Day {
+
+  SUNDAY,
+
+  MONDAY,
+
 }
 
-function work(d: Day) {
-    switch (d) {
-      case Day.SUNDAY:
-      case Day.SATURDAY:
-        return 'take a rest';
-      case Day.MONDAY:
-      case Day.TUESDAY:
-      case Day.WEDNESDAY:
-      case Day.THURSDAY:
-      case Day.FRIDAY:
-        return 'work hard';
-    }
+const work = (x: Day) => {
+
+  if (x === Day.SUNDAY) {
+
+    x; // 类型是 Day
+
   }
 
-var s: String;
-s = work(Day.FRIDAY);
-console.log(s);
-// 编译：ts-node 09.js 输出：work hard
-// tsc HelloWorld.ts --strict --alwaysStrict false --watch 实时转译输出 js
+}
---
+++ 10.ts
@@ -0,0 +1,6 @@
+// 泛型
+// tsc 10.ts --strict --alwaysStrict false --watch
+// ts-node 10.ts
+console.log("泛型");
+
+
